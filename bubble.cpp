2ebb60b35e301b387c81dcee8d2d2b9ee91ad11f
HighOnMelatonin <tanruianh@outlook.com>
2024-10-01 21:32:55 +0800

    Bubble sort in cpp

---
+++ bubble.cpp
@@ -0,0 +1,31 @@
+// bubble sort in C++
+
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+
+    int array[11] = {5, 16, 14, 10, 8, 7, 8, 3, 2, 4, 1};
+
+    for (int i = 0; i < 11; i++)
+    {
+        for (int j = 1; j < (10 - i); j++)
+        {
+            if (array[j] < array[j - 1])
+            {
+                // Swap values without 3rd variable (save space)
+                array[j] = array[j] + array[j - 1];
+                array[j - 1] = array[j] - array[j - 1];
+                array [j] = array[j] - array[j - 1];
+            }
+        }
+    }
+
+    for (int i = 0; i < 10; i++)
+    {
+        cout << array[i] << endl;
+    }
+
+    return 0;
+}
