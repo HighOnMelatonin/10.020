c779ea24c23571868753355cda7f6d5b4e22cab6
HighOnMelatonin <tanruianh@outlook.com>
2024-10-01 22:09:22 +0800

    Optimised insertion, reduced inner comparisons

--- insertion.cpp
+++ insertion.cpp
@@ -8,6 +8,7 @@ int main()
 
     int array[10] = {5, 16, 14, 10, 8, 7, 8, 3, 2, 4};
 
+    // Print before
     for (int i = 0; i < 10; i++)
     {
         cout << array[i] << " ";
@@ -18,16 +19,18 @@ int main()
     for (int i = 1; i < 10; i++)
     {
         int j = i;
-        while ((array[j] < array[j - 1]) and j > 0)
+        int current = array[j];
+
+        while ((current < array[j - 1]) and j > 0)
         {
             // Swap values without 3rd variable
-            array[j - 1] =  array[j] + array[j - 1];
-            array[j] = array[j - 1] - array[j];
-            array[j - 1] = array[j - 1] - array[j];
+            array[j] = array[j - 1];
             j -= 1;
         }
+        array[j] = current;
     }
 
+    // Print after
     for (int i = 0; i < 10; i++)
     {
         cout << array[i] << " ";
